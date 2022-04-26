f95a4e21baf664322cf6372325016388cf9004af
UbaldoFMelchor <ubaldofmelchor97@hotmail.com>
2022-04-26 00:13:01 -0500

    agregado test 2

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -13,5 +13,11 @@ describe("Unit Test for Spiderman class", () => {
         expect(andrewGarfield.actor).toBe("Andrew Garfield")
         expect(andrewGarfield.movies).toBe(2)
         expect(andrewGarfield.studio).toBe("Sony")
+
     });
+    test('2) Use the method getInfo()', () => {
+        const tomHolland = new Spiderman("Spiderman Marvel", 25, "Tom Holland", 5, "Marvel")
+        expect(tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel Studios")
+    });
+    
 })
