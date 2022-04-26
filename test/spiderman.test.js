857f03b3d619539a972816ef7ac549b8c3cb738b
UbaldoFMelchor <ubaldofmelchor97@hotmail.com>
2022-04-26 00:18:16 -0500

    corregido test despues de dar fail

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -17,7 +17,7 @@ describe("Unit Test for Spiderman class", () => {
     });
     test('2) Use the method getInfo()', () => {
         const tomHolland = new Spiderman("Spiderman Marvel", 25, "Tom Holland", 5, "Marvel")
-        expect(tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel Studios")
+        expect(tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel studios")
     });
     
 })
